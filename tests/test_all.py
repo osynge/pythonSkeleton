8215eaf8483c32a18e79c350cfd75f2f4007189c
Owen Synge <owen.synge@jaysnest.de>
2019-03-02 21:22:46 +0100

    Initial commit
    Signed-off-by: Owen Synge <osynge@googlemail.com>

---
+++ tests/test_all.py
@@ -0,0 +1,17 @@
+import logging
+import mock
+
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG)
+
+
+class TestPython4DepositSolutions():
+    def test_hasVersion(self):
+        from python_for_deposit_solutions.__version__ import version
+        versionLen = len(version.split('.'))
+        assert(3 == versionLen)
+
+    def test_hasVersion(self):
+        import python_for_deposit_solutions
+        python_for_deposit_solutions.main()
